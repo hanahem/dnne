7c33e99441c03f02a6dea19ba4db32a73d5533cd
if09036 <if09036@f54a5e4c-db9c-46af-ac38-2f024c6dc003>
2017-03-10 20:47:30 +0000

    This version contains: - Diagramming functionnalities - JSON dict. live updates - functions headers (encoders, other utilities)
    TODO:
    - add draggable layouts
    - complete functions' cores

---
+++ branches/prototype2/encoders.js
@@ -0,0 +1,43 @@
+/**
+ * This document contains encoders for each target library
+ * Each function encodes the JSON-String input, and
+ * maps it into a list of strings formatted in the target library's syntax
+ * The output list is a stack of command lines
+ * TODO: test-cases to verify commands execution
+ **/
+
+/**
+ * jsonToKeras: encodes JSON to Keras syntax
+ * IN: JSON-String model
+ * @return: stack list of strings (representing the command lines)
+ */ 
+function jsonToKeras(jsonModel) {
+  
+}
+
+/**
+ * jsonToTflow: encodes JSON to Tensorflow syntax
+ * IN: JSON-String model
+ * @return: stack list of strings (representing the command lines)
+ */ 
+function jsonToTflow(jsonModel) {
+  
+}
+
+/**
+ * jsonToTheano: encodes JSON to Theano syntax
+ * IN: JSON-String model
+ * @return: stack list of strings (representing the command lines)
+ */ 
+function jsonToTheano(jsonModel) {
+  
+}
+
+/**
+ * jsonToTorch: encodes JSON to Torch syntax
+ * IN: JSON-String model
+ * @return: stack list of strings (representing the command lines)
+ */ 
+function jsonToTorch(jsonModel) {
+  
+}
