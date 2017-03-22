7fd9609e219a1e05934503d215439b37beb5a503
if09036 <if09036@f54a5e4c-db9c-46af-ac38-2f024c6dc003>
2017-03-22 17:19:08 +0000

    commented checkboxManager.js

--- checkboxManager.js
+++ checkboxManager.js
@@ -1,9 +1,12 @@
+/**
+ * This script contains various functions managing the input and output layers' checkboxes
+ */
 //=================
 // Checkboxes MAN
 //=================
 
 /**
- * this function checks the input element by ID
+ * this function checks the input elemnt by ID
  */
 function check(id) {
     document.getElementById(id).checked = true;
@@ -23,26 +26,30 @@ function manageCheck(checkedId) {
   var x = document.getElementsByClassName("inCheck");
   var y = document.getElementsByClassName("outCheck");
   var i;
-  for (i = 0; i < x.length; i++) {
+  for (i = 0; i < x.length; i++) { //finds checked element in x, and uncheks all the others
       if(x[i].id != checkedId){
         uncheck(x[i].id);
       }
   }
-  for (i = 0; i < y.length; i++) {
+  for (i = 0; i < y.length; i++) { //finds checked element in y, and uncheks all the others
       if(y[i].id != checkedId){
         uncheck(y[i].id);
       }
   }
 }
 
+//=========================
+// Input or Output checking
+//=========================
 /**
  * this function checks whether a layer type is checked as input or not
  * in order to use addLayer() by choosing to add inservices or not
+ * @return: boolean, true if id is an checked as input layer, false otherwise
  */
 function isInputLayer(id) {
-  var x = document.getElementsByClassName("inCheck");
+  var x = document.getElementsByClassName("inCheck");//selct all the elements that represent input checkboxes
   var i;
-  for (i = 0; i < x.length; i++) {
+  for (i = 0; i < x.length; i++) { //loop to find if there's a checkbox with the activation name =to id, and if this element is checked
       var activation = x[i].id.slice(0, -1);
       if(activation == id && x[i].checked){
         return true;
@@ -51,10 +58,15 @@ function isInputLayer(id) {
   return false;
 }
 
+/**
+ * this function checks whether a layer type is checked as output or not
+ * in order to use addLayer() by choosing to add inservices or not
+ * @return: boolean, true if id is an checked as output layer, false otherwise
+ */
 function isOutputLayer(id) {
-  var x = document.getElementsByClassName("outCheck");
+  var x = document.getElementsByClassName("outCheck");//selct all the elements that represent output checkboxes
   var i;
-  for (i = 0; i < x.length; i++) {
+  for (i = 0; i < x.length; i++){ //loop to find if there's a checkbox with the activation name =to id, and if this element is checked
       var activation = x[i].id.slice(0, -1);
       if(activation == id && x[i].checked){
         return true;
