df6a17e1e646330ec0c868deaa161196d1bdecad
if09036 <if09036@f54a5e4c-db9c-46af-ac38-2f024c6dc003>
2017-04-07 18:29:05 +0000

    deleted checkboxManager.js deleted macrostructs.js

--- trunk/macrostructs.js
+++
@@ -1,6 +0,0 @@
-/**
- * This script contains functions that return premade
- * ANN structures (image classifier, CNN, RNN ...)
- * The functions return an array containing a nodeDataArray
- * and a LinkDataArray.
- */
